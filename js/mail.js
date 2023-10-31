58b1422ae0ffd2b2fccf68cb57fce77656f28f0f
Eugen <epopescuu@yahoo.it>
2023-10-31 16:23:39 +0100

    first commit

---
+++ js/mail.js
@@ -0,0 +1,29 @@
+const inputEmail = document.querySelector('#email');
+const btnVerify = document.querySelector('#myBtn');
+const outputEmail = document.querySelector('#output');
+
+const logers = ['andrei@gmail.com', 'victor@gmail.com', 'francesco@gmail.com', 'anatoli@gmail.com', 'marco@gmail.com', 'paras@gmail.com', 'ion@gmail.com', 'sabrina@gmail.com', 'alex@gmail.com', 'eugen@gmail.com', 'james@gmail.com', 'simo@gmail.com', 'pippo@gmail.com'];
+logers.push('epopescuu@yahoo.it');
+
+btnVerify.addEventListener('click', function() {
+    
+    if (inputEmail.value === '') {
+        alert('Please insert an email');
+    } else {
+        for (let i = 0; i < logers.length; i++) {
+            const email = logers[i];
+            
+            console.log(email);
+
+            if (email == inputEmail.value) {
+                document.querySelector('#output').style.color = "green"
+                document.querySelector('#output').style.fontSize = "2rem"
+                outputEmail.innerHTML = `Questa email "${email}" è Loggata`;
+            } else {
+                document.querySelector('#output').style.color = "red"
+                document.querySelector('#output').style.fontSize = "2rem"
+                outputEmail.innerHTML = `Non esiste "${inputEmail.value}", Registrati`;
+            }
+        }
+    }
+});
