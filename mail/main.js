eaf55020f3153bca837f060c78a4c68b74d933a8
Eugen <epopescuu@yahoo.it>
2023-10-31 16:46:04 +0100

    reordered

--- mail/main.js
+++ mail/main.js
@@ -3,7 +3,7 @@ const btnVerify = document.querySelector('#myBtn');
 const outputEmail = document.querySelector('#output');
 let verify = false;
 
-const logers = ['andrei@gmail.com', 'victor@gmail.com', 'francesco@gmail.com', 'anatoli@gmail.com', 'marco@gmail.com', 'paras@gmail.com', 'ion@gmail.com', 'sabrina@gmail.com', 'alex@gmail.com', 'eugen@gmail.com', 'james@gmail.com', 'simo@gmail.com', 'pippo@gmail.com'];
+let logers = ['andrei@gmail.com', 'victor@gmail.com', 'francesco@gmail.com', 'anatoli@gmail.com', 'marco@gmail.com', 'paras@gmail.com', 'ion@gmail.com', 'sabrina@gmail.com', 'alex@gmail.com', 'eugen@gmail.com', 'james@gmail.com', 'simo@gmail.com', 'pippo@gmail.com'];
 logers.push('epopescuu@yahoo.it');
 
 btnVerify.addEventListener('click', function() {
