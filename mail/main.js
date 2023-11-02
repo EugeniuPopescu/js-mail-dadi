410007fbb939a010185b4628a1ee54aaaeee798c
Eugen <epopescuu@yahoo.it>
2023-11-02 13:23:40 +0100

    perf:(main.js): add indexOF() & substring()

--- mail/main.js
+++ mail/main.js
@@ -1,10 +1,18 @@
 const inputEmail = document.querySelector('#email');
 const btnVerify = document.querySelector('#myBtn');
 const outputEmail = document.querySelector('#output');
-let verify = false;
 
 let logers = ['andrei@gmail.com', 'victor@gmail.com', 'francesco@gmail.com', 'anatoli@gmail.com', 'marco@gmail.com', 'paras@gmail.com', 'ion@gmail.com', 'sabrina@gmail.com', 'alex@gmail.com', 'eugen@gmail.com', 'james@gmail.com', 'simo@gmail.com', 'pippo@gmail.com'];
 logers.push('epopescuu@yahoo.it');
+
+let verify = false;
+
+// prendere solo il nome utente senza la parte finale della @gmail.com
+// indexOf() -> prende la posizione di un di un carattere che gli do
+// substring(min, max) -> mi taglia il carattere a seconda di come gli do min e max
+const chiocciola = (inputEmail.value).indexOf("@");
+let nomeUtente = (inputEmail.value).substring(0, chiocciola);
+
 
 btnVerify.addEventListener('click', function() {
     
@@ -14,22 +22,26 @@ btnVerify.addEventListener('click', function() {
         for (let i = 0; i < logers.length; i++) {
             const email = logers[i];
             
-            
+            // prendere solo il nome utente senza la parte finale della @gmail.com
+            // indexOf() -> prende la posizione di un di un carattere che gli do
+            // substring(min, max) -> mi taglia il carattere a seconda di come gli do min e max
+            const chiocciola = (inputEmail.value).indexOf("@");
+            let nomeUtente = (inputEmail.value).substring(0, chiocciola);
+
             if (email == inputEmail.value) {
+                verify = true;
+                
                 document.querySelector('#output').style.color = "green"
                 document.querySelector('#output').style.fontSize = "2rem"
-                outputEmail.innerHTML = `Questa email "${email}" è Loggata`;
-                console.log(email);
-                
-                verify = true;
+                outputEmail.innerHTML = `Benvenuto "${nomeUtente.toUpperCase()}"`;
 
                 break;
             } else {
+                verify = false;
+
                 document.querySelector('#output').style.color = "red"
                 document.querySelector('#output').style.fontSize = "2rem"
-                outputEmail.innerHTML = `Non esiste, Registrati`;
-
-                verify = false;
+                outputEmail.innerHTML = `"${(inputEmail.value).toUpperCase()}" NON sei registrato`;
             }
         }
     }
